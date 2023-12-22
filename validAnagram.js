2219bf12a296b44be531d3b698c92651bbd0f986
Iris Jeong <77691556+iris-jeong@users.noreply.github.com>
2023-12-22 18:27:37 -0500

    Return true if t is an anagram of s, and false otherwise

---
+++ validAnagram.js
@@ -0,0 +1,26 @@
+/* 
+  -- INSTRUCTIONS --
+  Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+  An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+  
+  Example 1:
+  Input: s = "anagram", t = "nagaram"
+  Output: true
+  
+  Example 2:
+  Input: s = "rat", t = "car"
+  Output: false
+  
+  Constraints:
+  1 <= s.length, t.length <= 5 * 104
+  s and t consist of lowercase English letters.
+*/
+
+/* SOLUTION */
+// eslint-disable-next-line no-unused-vars
+var isAnagram = function (s, t) {
+	const sSorted = s.split('').sort().join('');
+	const tSorted = t.split('').sort().join('');
+
+	return sSorted === tSorted;
+};
