8007e8c7bb4e11d7e3e56d358fa11caaaf044c25
Iris Jeong <77691556+iris-jeong@users.noreply.github.com>
2024-09-16 14:19:15 -0700

    Updated solution with comments

--- removeNthNode.js
+++ removeNthNode.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* 
   -- INSTRUCTIONS --
@@ -26,3 +27,38 @@ function removeNthLastNode(head, n) {
 
 	return head;
 }
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+	// Create a dummy node to handle edge cases where the head might be removed.
+	let dummy = new ListNode(0, head);
+	let slow = dummy;
+	let fast = dummy;
+
+	// Move the fast pointer n steps ahead.
+	for (let i = 0; i < n; i++) {
+		fast = fast.next;
+	}
+
+	// Move both fast and slow pointers until fast reaches the end.
+	while (fast.next !== null) {
+		slow = slow.next;
+		fast = fast.next;
+	}
+
+	// Slow is now at the node before the one we want to remove.
+	slow.next = slow.next.next;
+
+	return dummy.next;
+};
